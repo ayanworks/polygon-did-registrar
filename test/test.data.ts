bc771aa40bc90a4a28133eec48f48697eaabdb5b
anushaGarg <anusha.garg@ayanworks.com>
2022-08-02 18:18:59 +0530

    Added registry contract address

--- test/test.data.ts
+++ test/test.data.ts
@@ -1,12 +1,12 @@
 
-export const testDid = 'did:polygon:0x0f03e85c11e1c832c994777bdf610425c30eeba2';
+export const testDid = 'did:polygon:0x36DE07674a8A1ffef7cfE96BeFCE9b48107a3F92';
 
 export const updateDidDocument = "{\"@context\":\"https://w3id.org/did/v1\",\"id\":\"did:polygon:0x0c63b671f7215a764665874f192cde0c0b6f6326\",\"verificationMethod\":[{\"id\":\"did:polygon:0x0c63b671f7215a764665874f192cde0c0b6f6326\",\"type\":\"EcdsaSecp256k1VerificationKey2019\",\"controller\":\"did:polygon:0x0c63b671f7215a764665874f192cde0c0b6f6326\",\"publicKeyBase58\":\"NxkSq85Fq2hdev5hE6tCFMsF7yjnEwBtGFJU1RtGXykUwgTY4MfSaCGC1HQFoDmQYLPTy38M1nThz9gWgAVgtWa6\"}]}";
 
-export const privateKey = "0xd922b5e24ab1132f68d8c3a3e758d6814f71d5b1acffdffa6a19b8f8d55db507";
+export const privateKey = "0x463899069514e325783d2dba6001a3fc6c803224ccfb3474135c3e1c0de001ba";
 
-export const url = "https://rpc-mumbai.matic.today";
+export const url = "https://polygon-mumbai.g.alchemy.com/v2/U-coePatRQJjq7qA3xo3KZvcPBwf9JtG";
 
-export const contractAddress = "0x36863563fF027E9590837CD377bBCCB862E342B7";
+export const contractAddress = "0x3463F1f3D5639AFA5507493ffF207be292aC7E33";
 
 export const network = "testnet";
