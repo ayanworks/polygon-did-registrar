0042eba4b7e89a3ef0913565329191d6cfd10a2b
anushaGarg <anusha.garg@ayanworks.com>
2022-11-11 14:58:27 +0530

    Changed Registry contract package

--- test/test.data.ts
+++ test/test.data.ts
@@ -5,7 +5,7 @@ export const updateDidDocument = "";
 
 export const privateKey = "";
 
-export const url = "";
+export const url = "https://rpc-mumbai.maticvigil.com/";
 
 export const contractAddress = "0x8B335A167DA81CCef19C53eE629cf2F6291F2255";
 
